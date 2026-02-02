15e67acac2432b693036760dedecfaf5019dfecd
xkazm04 <kazdan@seznam.cz>
2026-02-02 15:23:47 +0100

    feat(01-04): create barrel export for template discovery feature
    - Exports TemplateDiscoveryPanel component
    - Exports all API functions from discoveryApi

---
+++ src/app/features/Integrations/sub_TemplateDiscovery/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Template Discovery Feature
+ * Exports for the template discovery UI
+ */
+
+export { TemplateDiscoveryPanel } from './TemplateDiscoveryPanel';
+export * from './lib/discoveryApi';
